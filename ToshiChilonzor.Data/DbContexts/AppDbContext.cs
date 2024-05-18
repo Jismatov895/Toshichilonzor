5a2d1a7306c194736246fd7d4f3dace6ac633230
axrordev <axror.algo@gmail.com>
2024-05-19 00:03:06 +0500

    created n tier architecture

---
+++ ToshiChilonzor.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToshiChilonzor.Data.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=localhost; Database=Airway; Port=5432; User ID=postgres; Password=1001");
+    }
+}
